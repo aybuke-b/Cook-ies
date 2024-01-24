4869e4ccdd154235df10bf3e0671a91dc06428d2
Guillaume DEVANT <guillaume.devant@outlook.com>
2024-01-24 17:41:30 +0100

    scraping de génie de cuisneaz

--- scraping_az.py
+++ scraping_az.py
@@ -1,6 +1,40 @@
 from bs4 import BeautifulSoup as BS
 import requests as rq
 from Recette import Recette
+import re
+import numpy as np
+
+def get_pays(url:str) -> list[str]:
+    res = rq.get(url)
+    soupe = BS(res.text, 'html.parser')
+    pays = [a['href'] for a in soupe.select('div.tile_content a.tile_title')]
+    urls = []
+    for pay in pays:
+        urls.append("https://www.cuisineaz.com" + pay)
+    return urls
+
+def get_page_pays(urls_pays:str) -> int:
+    str_page = _soupage(urls_pays[0]).find_all("a", "lienPagination")[-1].text
+    nb_page = int(re.findall(r'\d+', str_page)[0])
+    return nb_page
+
+def get_url_pays(lien:str, nb_page:int) -> list[str]:
+    urls_in_pays = []
+    for page in range(1,int(nb_page) + 1):
+        urls_in_pays.append(lien + f"/{page}/")
+    return urls_in_pays
+
+def get_url_recette(urls_in_pays:str) -> list[str]:
+    recettes_pays = []
+    for url in urls_in_pays:
+        blocs  = _soupage(url).find_all("section", class_ = "block-primary tiles --col-2 md:--col-4")
+        for bloc in blocs:
+            recettes = bloc.find_all("article")
+            for recette in recettes:
+                href = recette.find("a").get("href")
+                recettes_pays.append("https://www.cuisineaz.com" + href)
+    return list(np.unique(recettes_pays))
+    
 
 def _soupage(lien:str) -> BS:
     requete = rq.get(lien, timeout=1000)
@@ -35,3 +69,14 @@ def scrap(lien:str) -> Recette:
         nb_comment = _scrap_nb_comment(soupe)
         )
     return res
+
+def get_pipeline(url:str):
+    urls_pays = get_pays(url)
+    nb_page = get_page_pays(urls_pays)
+    urls_in_pays = get_url_pays(urls_pays[0], nb_page)
+    lien_recette_pays = get_url_recette(urls_in_pays)
+    for lien in lien_recette_pays:
+        try:
+            print(scrap(lien))
+        except:
+            pass
