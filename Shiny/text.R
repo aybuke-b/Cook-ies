801217d081ff5152c1c8c902c0d29e7df5d0968e
Guillaume DEVANT <guillaume.devant@outlook.com>
2024-02-05 18:07:56 +0100

    sentiment analysis

--- Shiny/text.R
+++ Shiny/text.R
@@ -8,21 +8,82 @@ library(wordcloud)
 library(gutenbergr)
 library(FactoMineR)
 library(janitor)
+library(arrow)
 
-
-df <- read.csv("data/comment.csv",
+df <- read.csv("data/comment_en.csv",
                sep = ",",
                header = TRUE,
                fileEncoding = "utf-8")
+df <- df[,-c(3)]
 
 liste_perso <- c("a")
 
+## SENTIMENT
+
+### afinn
+score_afinn <- df |> select("comment_en", "nom") |>
+  unnest_tokens(word,comment_en) |> 
+  inner_join(get_sentiments("afinn")) |>
+  group_by(nom) |>
+  summarise(sentiment = sum(value))
+
+### bing
+score_bing <- df |> select("comment_en", "nom") |>
+  unnest_tokens(word,comment_en) |> 
+  inner_join(get_sentiments("bing")) |> 
+  group_by(nom) |> 
+  summarise(
+    positive = sum(sentiment == "positive"),
+    negative = sum(sentiment == "negative"))
+
+score_bing <- score_bing |> 
+  mutate(ratio = positive / (negative + positive)) 
+
+bing_count <- df |> select("comment_en", "nom") |>
+  unnest_tokens(word,comment_en) |> 
+  inner_join(get_sentiments("bing")) |> 
+  count(word, sentiment, sort = TRUE)
+
+bing_count |> 
+  group_by(sentiment) |> 
+  top_n(10) |> 
+  ungroup() |> 
+  ggplot(aes(x = reorder(word, n), y = n)) + 
+  geom_col(fill = "orchid", alpha = 0.6) +
+  facet_wrap(~sentiment, nrow= 1, scale = "free") + 
+  coord_flip()+
+  labs(x = "word")
+
+### nrc
+score_nrc <- df |> select("comment_en", "nom") |>
+  unnest_tokens(word,comment_en) |> 
+  inner_join(get_sentiments("nrc")) 
+
+nrc_count <- df |> select("comment_en", "nom") |>
+  unnest_tokens(word,comment_en) |> 
+  inner_join(get_sentiments("nrc")) |> 
+  count(word, sentiment, sort = TRUE)
+
+nrc_count |> 
+  group_by(sentiment) |> 
+  top_n(10) |> 
+  ungroup() |> 
+  ggplot(aes(x = reorder(word, n), y = n)) + 
+  geom_col(fill = "orchid", alpha = 0.6) +
+  facet_wrap(~sentiment, nrow= 1, scale = "free") + 
+  coord_flip()+
+  labs(x = "word")
+
+#score = merge(score_afinn, score_bing[,-c(2,3)], by = "nom")
+
+### TEXTDATA
+
 res_td <- TextData(df,
-         var.text = c("comment"),
-         idiom = "fr",
+         var.text = c("comment_en"),
+         idiom = "en",
          remov.number = FALSE,
          context.quanti = "cout",
-         context.quali = c(1,4),
+         context.quali = c(1,3),
          segment = TRUE,
          seg.nfreq = 4,
          seg.nfreq2 = 10,
@@ -35,15 +96,17 @@ summary(res_td)
 
 plot(res_td,
      nword = 40,
-     sel = "seg",
+     sel = "word",
      title = "Les 10 mots les plus fréquents",
      xtitle = "Fréquences",
      col.fill = "azure2")
 
-res_lexCA <- LexCA(res_td, graph = FALSE)
 
-plot(res_lexCA,
-     gtype = "DocWord",
-     title = "Représentation graphique des mots",
-     col.word = "orange",
-     col.doc = "royalblue")
+
+#res_lexCA <- LexCA(res_td, graph = FALSE)
+#
+#plot(res_lexCA,
+#     gtype = "DocWord",
+#     title = "Représentation graphique des mots",
+#     col.word = "orange",
+#     col.doc = "royalblue")
---
+++ Shiny/translate.R
@@ -0,0 +1,21 @@
+library(gtranslate)
+
+df <- read_parquet("data/recette.parquet")
+df_comment <- df[,c(1,3,10,11)] |> unnest("comment")
+
+comment_en = list()
+
+comment_en <- lapply(
+  df_comment$comment, 
+    function(comment) {
+    translate(
+        text = comment, 
+        from = "fr",
+        to = "en"
+        )
+    }
+)
+
+df_comment$comment_en <- unlist(comment_en)
+
+write.csv(df_comment, "./data/comment_en.csv", row.names=FALSE)
